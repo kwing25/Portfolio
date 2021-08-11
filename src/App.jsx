f44cc9eea485d2f43745183a9cd917472f8fe9c1
Kendra Wing <kendracwing@icloud.com>
2021-08-11 02:16:23 -0600

    Edited paths and app pages structure
    Edited and added import paths. Created new 'export default function App' for website pages structure.

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "./App.css";
 
 import Nav from "./components/Nav.jsx";
-
-import Home from "./pages/Home.jsx"
+import Home from "./pages/Home.jsx";
 import Contact from "./pages/Contact.jsx";
-// import About from "./components/About";
-// import Skills from "./components/Skills";
-// import Projects from "./components/Projects";
+import About from "./pages/About.jsx";
+import Portfolio from "./pages/Portfolio.jsx";
 
 export default function App() {
+  const [currentPage, setCurrentPage] = useState("Home");
+
+  // This method is checking to see what the value of `currentPage` is. Depending on the value of currentPage, we return the corresponding component to render.
+  const renderPage = () => {
+    if (currentPage === "Home") {
+      return <Home />;
+    }
+    if (currentPage === "About") {
+      return <About />;
+    }
+    if (currentPage === "Portfolio") {
+      return <Portfolio />;
+    }
+    return <Contact />;
+  };
+
+  const handlePageChange = (page) => setCurrentPage(page);
+
   return (
-    <main>
-      <Home/>
-      <Nav />
-      {/* <About />
-      <Projects />
-      <Skills /> */}
-      {/* <Testimonials /> */}
-      <Contact />
-    </main>
+    <div>
+      {" "}
+      {/* We are passing the currentPage from state and the function to update it */}{" "}
+      <Nav currentPage={currentPage} handlePageChange={handlePageChange} />{" "}
+      {/* Here we are calling the renderPage method which will return a component  */}{" "}
+      {renderPage()}{" "}
+    </div>
   );
 }
+
+// export default function App() {
+//   return (
+//     <main>
+//       <Nav />
+//       <Home />
+//       {/* <About />
+//       <Projects />
+//       <Skills /> */}
+//       {/* <Testimonials /> */}
+//       <Contact />
+//     </main>
+//   );
+// }
 
 
 // import './App.css';
