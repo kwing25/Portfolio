b5ffc49bb40258e9a8b9ec18fbfe245af59b7ee9
Kendra Wing <kendracwing@icloud.com>
2021-08-10 23:18:33 -0600

    Created site pages
    Home, About, Skills, Portfolio, Contact pages

---
+++ src/pages/Contact.js
@@ -0,0 +1,34 @@
+import React from "react";
+
+
+export default function Contact() {
+    return (
+        <div class="contact" id="contactpage">
+            <h1>Contact Me</h1>
+
+            <div class="container">
+                <div class="contact-info">
+                    <ul class="contact-info_items">
+                        <li class="email">
+                            <a href="../1MainFiles/index.html">
+                                <i class="fas fa-envelope"></i> Email: kendrawingpro@icloud.com
+              </a>
+                        </li>
+                        <li class="linkedinaccount">
+                            <a href="https://www.linkedin.com/in/kendrawing/">
+                                <i class="fab fa-linkedin-in"></i> LinkedIn Account:
+                www.linkedin.com/in/kendrawing
+              </a>
+                        </li>
+                        <li class="githubaccount">
+                            <a href="https://github.com/kwing25">
+                                <i class="fab fa-github"></i>
+                GitHub Account: github.com/kwing25
+              </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    );
+}
---
+++ src/pages/Home.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+
+export default function Home() {
+    return ( 
+        <div class="home" id="homepage">
+        <h1>Hello My Name is Kendra👋. I'm a Web Developer💻. This is my Portfolio💼.</h1>
+        </div>
+    );
+}
