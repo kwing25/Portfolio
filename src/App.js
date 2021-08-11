5f032091676f11dbe97f14004c68078f0343b8a0
Kendra Wing <kendracwing@icloud.com>
2021-08-10 23:27:56 -0600

    Edited files- paths and comments, etc.
    Edited paths and removed unnecessary lines...

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import './App.css';
+import React from "react";
 
-function App() {
+import "./App.css";
+
+import Nav from "./components/Nav";
+
+import Home from "./pages/Home"
+import Contact from "./pages/Contact";
+// import About from "./components/About";
+// import Skills from "./components/Skills";
+// import Projects from "./components/Projects";
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <Home/>
+      <Nav />
+      {/* <About />
+      <Projects />
+      <Skills /> */}
+      {/* <Testimonials /> */}
+      <Contact />
+    </main>
   );
 }
 
-export default App;
+
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
