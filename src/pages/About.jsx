b3639018232f638b338defee031fc9404e824bc8
Kendra Wing <kendracwing@icloud.com>
2021-08-13 16:56:52 -0600

    Added content
    Added additional content to site pages.

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -7,9 +7,17 @@ export default function About() {
 
             <div class="container">
         <h1 id="page-title">About Me</h1>
-
-
         <h1>Page Under Construction🛠</h1>
+
+        <p>I’m a Full Stack Flex Coding Student at University of Denver.
+                        I'm Front-End Developer. I have a passion for UI effects and love creating colorful and creative
+                        websites.
+                        Very organized and an independent employee with a high attention to detail. Love computers and
+                        tech.
+                        Big
+                        fan of
+                        Comic Books and Sci-Fi. Proud dog owner/lover.
+                    Hugely interested in the frontend spectrum and love creating interesting projects.</p>        
         </div>
         </div>
 
--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -3,14 +3,36 @@ import React from "react";
 
 export default function Portfolio() {
     return (
-        <div class="about" id="aboutpage">
+        <div class="portfolio" id="portfoliopage">
 
             <div class="container">
                 <div class="row">
                     <h1 id="page-title">Portfolio</h1>
-
-
                     <h1>Page Under Construction🛠</h1>
+
+        <div className="my-work">
+            <div className="row row-cols-1 row-cols-md-3 g-4">
+                <div className="card">
+                    <div className="card-body">
+                        <h5 class="card-title">Card #1</h5>
+                        <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin eget tortor risus. Quisque velit nisi, pretium ut lacinia in, elementum id enim. Praesent sapien massa, convallis a pellentesque nec, egestas non nisi. Nulla quis lorem ut libero malesuada feugiat. Cras ultricies ligula sed magna dictum porta.</p>
+                    </div>
+                </div>
+                <div className="card">
+                    <div className="card-body">
+                        <h5 class="card-title">Card #1</h5>
+                        <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin eget tortor risus. Quisque velit nisi, pretium ut lacinia in, elementum id enim. Praesent sapien massa, convallis a pellentesque nec, egestas non nisi. Nulla quis lorem ut libero malesuada feugiat. Cras ultricies ligula sed magna dictum porta.</p>
+                    </div>
+                </div>
+                <div className="card">
+                    <div className="card-body">
+                        <h5 class="card-title">Card #1</h5>
+                        <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin eget tortor risus. Quisque velit nisi, pretium ut lacinia in, elementum id enim. Praesent sapien massa, convallis a pellentesque nec, egestas non nisi. Nulla quis lorem ut libero malesuada feugiat. Cras ultricies ligula sed magna dictum porta.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
                 </div>
             </div>
         </div>
