55d5b8f19fb9afd80adfba2237f2497bab564e88
Kendra Wing <kendracwing@icloud.com>
2021-08-14 21:36:54 -0600

    Created social nav component for site
    Social nav links for site.

---
+++ src/components/socialNav.jsx
@@ -0,0 +1,26 @@
+// import React from "react"
+
+// export default function socialNav() {
+//     return (
+//         <div className="social-nav">
+//             <div className="netbox">
+//                 <a
+//                     className="icon ion-social-facebook"
+//                     data-tip="I share some interesting posts about Tech in FB!"
+//                     href="#"
+//                 />
+//                 <a
+//                     className="icon ion-social-twitter"
+//                     data-tip="Wanna know when I'm cooking a new Codepen recipe? Follow me!"
+//                     href="#"
+//                 />
+//                 <a
+//                     className="icon ion-social-github"
+//                     data-tip="I've written a lot of lines of code, take a look :)"
+//                     href="#"
+//                 />
+//                 <a className="icon ion-social-codepen" data-tip="I do some Pens to have fun!" href="#" />
+//             </div>
+//         </div>
+//     )
+// }
