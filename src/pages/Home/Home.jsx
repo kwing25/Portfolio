46a4f1fe51267c28a9af50408c40b0f437bc33cd
Kendra Wing <kendracwing@icloud.com>
2021-08-15 15:27:48 -0600

    Restructured repo folders/files
    Created individual folders for pages/components and their styles. Easier to navigate. Updated paths.

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,14 +1,10 @@
 import React from "react"
-import "../styles/home.scss"
-
+import "../Home/home.scss"
 
 export default function Home() {
     return (
         <div class="home" id="homepage">
             <div class="container">
-
-
-
                 <h1 class="title-typing">Hello My Name is Kendra.</h1>
                 <h3>I love to build websites. </h3>
                 <h4>Websites that are... Colorful, Unique, Imaginative, Impactful</h4>
@@ -16,3 +12,4 @@ export default function Home() {
         </div>
     );
 }
+
--- src/pages/Portfolio/Portfolio.jsx
+++ src/pages/Portfolio/Portfolio.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "../styles/portfolio.scss"
+import "../Portfolio/portfolio.scss"
 
 export default function Portfolio() {
     return (
