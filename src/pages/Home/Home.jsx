aa72ce42d6e00b3f3e552a9d72efc60f4d0eada9
Kendra Wing <kendracwing@icloud.com>
2021-08-17 17:07:08 -0600

    Updated / Edited Files
    Added more styling and edited home page structure.

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -6,32 +6,30 @@ export default function Home() {
         <div class="home" id="homepage">
 
             <div class="container">
-                <div class="row justify-content-md-center" id="intro-row">
-                    <h1 class="title-typing">Hello My Name is Kendra.</h1>
-                </div>
-            </div>
 
-            <div class="container">
-                <div class="row row-cols-2">
-                    <div class="col-6">
+                <div class="row justify-content-center">
+                    <div class="col-md-12">
+                        <h1 class="title-sentence">Hello My Name is Kendra.</h1>
+                    </div>
+                </div>
+
+                <div class="row justify-content-center" id="main">
+                    <div class="col-md-6">
                         <h3>I love to build websites. </h3>
+                    </div>
+                    <div class="col-md-6">
                         <h4>Websites that are... Colorful, Unique, Imaginative, Impactful</h4>
                     </div>
-                    <div class="col-6">
-                        <div class="profile-card"><img src="https://raw.githubusercontent.com/kwing25/New-Portfolio/main/src/assets/portraitblackwhite.jpg?token=ASYPV6AP27BHRNX4TIQH2PDBELF2U" class="img img-responsive" alt="profile-pic"></img>
-                            <div class="profile-name">Kendra Wing</div>
-                            <div class="profile-username">@kwing25</div>
-                            <div class="profile-icons">
-                                <a href="www.linkedin.com/in/kendrawing"><i class="fab fa-linkedin"></i></a>
-                                <a href="www.gitub.com/kwing25"><i class="fab fa-github-square"></i></a>
+                </div>
 
-                            </div>
-                        </div>
+                <div class="wrapper">
+                    <div class="pic-graphic">
+                        <div class="pic-graphic__img"></div>
                     </div>
                 </div>
 
             </div>
+
         </div>
     );
 }
-
