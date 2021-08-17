f9ac1401d34c35beefd28c48ed5e2e33cae960f6
Kendra Wing <kendracwing@icloud.com>
2021-08-17 17:07:55 -0600

    Edited files
    Working on nav and social links...

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "../Nav/nav.scss"
+import "../Nav/nav.scss";
 
 // Here we are using object destructuring assignment to pluck off our variables from the props object
 // We assign them to their own variable names
@@ -55,6 +55,8 @@ function Nav({ currentPage, handlePageChange }) {
         </li>{" "}
       </ul>
     </div>
+ 
+
   );
 }
 
--- src/components/socialNav/socialNav.jsx
+++ src/components/socialNav/socialNav.jsx
@@ -1,26 +1,41 @@
-// import React from "react"
+// import React from "react";
+// import "../socialNav/socialNav.scss";
+
 
 // export default function socialNav() {
 //     return (
 //         <div className="social-nav">
-//             <div className="netbox">
-//                 <a
-//                     className="icon ion-social-facebook"
-//                     data-tip="I share some interesting posts about Tech in FB!"
-//                     href="#"
-//                 />
-//                 <a
-//                     className="icon ion-social-twitter"
-//                     data-tip="Wanna know when I'm cooking a new Codepen recipe? Follow me!"
-//                     href="#"
-//                 />
-//                 <a
-//                     className="icon ion-social-github"
-//                     data-tip="I've written a lot of lines of code, take a look :)"
-//                     href="#"
-//                 />
-//                 <a className="icon ion-social-codepen" data-tip="I do some Pens to have fun!" href="#" />
-//             </div>
+//             <ul>
+//                 <li><a href="www.github.com/kwing25">Github</a></li>
+//             </ul>
 //         </div>
 //     )
 // }
+
+// // export default socialNav;
+
+// function socialNav({ currentPage, handlePageChange }) {
+//     return (
+//         <div class="socialNav">
+//             <ul className="nav nav-tabs">
+//                 <li className="nav-item">
+//                     <a
+//                         href="www.github.com/kwing25"
+//                     >          Github
+//                     </a>
+//                 </li>
+//                 <li className="nav-item">
+//                     <a
+//                         href="www.github.com/kwing25"
+//                     >          Github
+//                     </a>
+//                 </li>
+
+//             </ul>
+//         </div>
+
+
+//     );
+// }
+
+// export default socialNav;
