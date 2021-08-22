f24200aa689f6a638516a063ab5565ccbf46c9e5
Kendra Wing <kendracwing@icloud.com>
2021-08-22 17:26:48 -0600

    Changed app path
    Changed ReactDOM back App (used Proxy for placeholder/under constriction page).

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 // import './index.css';
-// import App from './App.jsx';
-import Proxy from "./Proxy";
+import App from './App.jsx';
+// import Proxy from "./Proxy";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Proxy />
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
